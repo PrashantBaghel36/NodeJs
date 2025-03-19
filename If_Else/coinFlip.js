af7a43463749f2e906b81b43aaf22227e097120d
Pesso <prashant9193034797@gmail.com>
2025-03-19 15:40:17 +0530

    JS

--- If_Else/coinFlip.js
+++
@@ -1,5 +0,0 @@
-function flipCoin() {
-    return Math.random() < 0.5 ? "Heads" : "Tails";
-}
-
-console.log(flipCoin());
--- Sequence_Problems/RandFun.js
+++
@@ -1,2 +0,0 @@
-let rand = Math.floor(Math.random() * 10);
-console.log(rand);
